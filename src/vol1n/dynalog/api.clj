002d5eb62d1f433fbcfbbe05fd73032dc7c8315d
Colin Ryan <colin.ryan.ga+git@gmail.com>
2025-03-08 11:18:17 -0500

    added pull to api.clj

--- src/vol1n/dynalog/api.clj
+++ src/vol1n/dynalog/api.clj
@@ -1,10 +1,12 @@
 (ns vol1n.dynalog.api
     (:require [vol1n.dynalog.core :as core]
               [vol1n.dynalog.transact :as tx]
-              [vol1n.dynalog.query :as q]))
+              [vol1n.dynalog.query :as q]
+              [vol1n.dynalog.pull :as pull]))
 
 (def transact tx/transact)
 (def client core/client)
+(def pull pull/pull)
 (def q q/q)
 (def connect core/connect)
 (def db core/db)
